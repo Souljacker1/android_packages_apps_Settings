346907c3d5ead8bbad5a62e423723a119f98c4c1
minarypenguin <alexfinhart@gmail.com>
2022-07-02 06:59:57 +0800

    Lab: Refactor renderer switch
    * some legacy devices graphics stack doesnt support skiagl nor skiavk out of the box (OEM stuffs)
    * replace skiagl with opengl to avoid glitchy UI rendering for devices with graphics stack that doesnt support skiavk/skiagl out of the box.
    * replace renderer backend too for compatibility, imo using skia backend alongside with opengl is a not a good idea unless blobs supports it.
    
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>

--- src/com/android/settings/laboratory/SkiavkPreferenceController.java
+++ src/com/android/settings/laboratory/SkiavkPreferenceController.java
@@ -23,8 +23,11 @@ import com.android.settingslib.development.SystemPropPoker;
 public class SkiavkPreferenceController extends TogglePreferenceController {
 
     private static final String PROPERTY_RENDERER = "persist.sys.hwui.renderer";
+    private static final String PROPERTY_RENDERER_ENGINE = "persist.sys.renderengine.backend";
     private static final String PROPERTY_USES_VULKAN = "ro.hwui.use_vulkan";
-    private static final String SKIAGL = "skiagl";
+    private static final String OPENGL_THREADED = "threaded";
+    private static final String SKIA_THREADED = "skiaglthreaded";
+    private static final String OPENGL = "opengl";
     private static final String SKIAVK = "skiavk";
 
     public SkiavkPreferenceController(Context context, String key) {
@@ -47,7 +50,8 @@ public class SkiavkPreferenceController extends TogglePreferenceController {
 
     @Override
     public boolean setChecked(boolean isChecked) {
-        SystemProperties.set(PROPERTY_RENDERER, isChecked ? SKIAVK : SKIAGL);
+        SystemProperties.set(PROPERTY_RENDERER, isChecked ? SKIAVK : OPENGL);
+        SystemProperties.set(PROPERTY_RENDERER_ENGINE, isChecked ? SKIA_THREADED : OPENGL_THREADED);
         SystemPropPoker.getInstance().poke();
         return true;
     }
